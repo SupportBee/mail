be3f814f3bb78e81dff8a113391b691afb35109f
josuemontano <josuemontanoa@gmail.com>
2015-12-01 13:12:25 -0400

    Refactor and keep LF in body

--- lib/monkeys/message.rb
+++ lib/monkeys/message.rb
@@ -1,9 +1,7 @@
 module Mail
   class Message
-    def parse_message
-      header_part, body_part = raw_source.lstrip.split(/#{CRLF}#{CRLF}|#{CRLF}#{WSP}{2,}#{CRLF}/m, 2)
-      self.header = header_part
-      self.body = body_part
-    end
+
+    HEADER_SEPARATOR = /#{CRLF}#{CRLF}|#{CRLF}#{WSP}{2,}#{CRLF}(?!#{WSP})/m
+
   end
 end
--- spec/monkeys/message_spec.rb
+++ spec/monkeys/message_spec.rb
@@ -4,14 +4,14 @@ describe Mail::Message do
   describe 'splitting' do
     it "doesn't split the message at an empty folded header" do
       message = Mail::Message.new("To: foo\r\n \r\nFrom: bar\r\n\r\nbody\r\n")
-      message.decoded.should == 'body'
+      message.decoded.should == "body\n"
     end
 
     # Borrowed from
     # https://github.com/mikel/mail/commit/a2a45597bce66ebe788cedaaab848a37bd04b25a
     it 'splits only once if there are "\r\n\r\n"s in the body' do
       message = Mail::Message.new("To: Example <example@cirw.in>\r\n\r\nHello\r\n\r\nthere\r\n")
-      message.decoded.should == "Hello\n\nthere"
+      message.decoded.should == "Hello\n\nthere\n"
     end
 
     # Note: Non RFC Conformant
@@ -20,7 +20,7 @@ describe Mail::Message do
     # rspec ./spec/mail/message_spec.rb:274 # Mail::Message accepting a plain text string email should give allow for whitespace on the gap line between header and body
     it 'allows 2 or more whitespaces on the gap line between header and body' do
       message = Mail::Message.new("To: foo\r\n  \r\nbody\r\n")
-      message.decoded.should == 'body'
+      message.decoded.should == "body\n"
     end
   end
 end
